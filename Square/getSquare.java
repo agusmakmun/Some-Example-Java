29fba74674ee2ce2b2127a97e91888d6d3727fae
agus makmun <summon.agus@gmail.com>
2015-05-28 01:04:21 +0700

    Create getSquare.java

---
+++ Square/getSquare.java
@@ -0,0 +1,13 @@
+/*
+This script for call class of Square.java
+@author Summon Agus (L200130113)
+@blog bloggersmart.net
+*/
+ 
+public class getSquare {
+	//pemanggilan
+	public static void main(String [] args) {
+		SegiEmpat semua = new SegiEmpat();
+		semua.showValue();
+	}
+}
