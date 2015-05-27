4aa7568af59169100718289e6bc956cf6c8dccd9
agus makmun <summon.agus@gmail.com>
2015-05-28 02:13:03 +0700

    Create cekIndonesia.java

---
+++ Abstract/cekIndonesia.java
@@ -0,0 +1,20 @@
+/**
+ * @author agaust
+ * @blog bloggersmart.net - python.web.id
+ */
+import java.util.Scanner;
+
+public class cekIndonesia {
+    public static void main (String [] args) {
+        SoureIndonesia indo = new SoureIndonesia("Indonesia");
+        indo.Kebangsaan();
+    }
+}
+
+/*Output:
+run:
+Indonesia raya adalah lagu kebangsaan republik Indonesia
+Indonesia raya pertama kali diumumkan pada upacara bendera 17 Agustus 1945
+Indonesia raya dipilih sebagai lagu kebangsaan ketika Indonesia memproklamasikan kemerdekaan.
+BUILD SUCCESSFUL (total time: 1 second)
+*/
