65ea7274517a8c6abfc81352ea03edaa4404cf0f
agus makmun <summon.agus@gmail.com>
2015-05-28 01:55:55 +0700

    Create helloworld.java

---
+++ Bicycle/helloworld.java
@@ -0,0 +1,26 @@
+/**
+ * @author agaust
+ * @blog bloggersmart.net - python.web.id
+ */
+
+public class helloworld {
+    public static void main (String [] arg) {
+        System.out.println("Selamat Datang");
+        
+        Bicycle bike1 = new Bicycle();
+        Bicycle bike2 = new Bicycle();
+        
+        bike1.changeCadance(50);
+        bike1.speedUp(10);
+        bike1.changeGear(2);
+        bike1.printStates();
+        
+        bike2.changeCadance(50);
+        bike2.speedUp(10);
+        bike2.changeGear(2);
+        bike2.changeCadance(40);
+        bike2.speedUp(10);
+        bike2.changeGear(3);
+        bike2.printStates();
+    }
+}
