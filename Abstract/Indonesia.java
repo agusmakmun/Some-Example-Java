684993033176bf8ab60cc8d8254d21d64316efe2
agus makmun <summon.agus@gmail.com>
2015-05-28 02:11:21 +0700

    Create Indonesia.java

---
+++ Abstract/Indonesia.java
@@ -0,0 +1,9 @@
+/**
+ * @author agaust
+ * @blog bloggersmart.net - python.web.id
+ */
+public abstract class Indonesia {
+    protected String indonesiaKu;
+    
+    public abstract void Kebangsaan();
+}
