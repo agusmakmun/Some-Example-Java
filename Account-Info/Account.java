757dd7256687af7a8843af22ddb37a2576d5acef
agus makmun <summon.agus@gmail.com>
2015-05-28 01:33:06 +0700

    Create Account.java

---
+++ Account-Info/Account.java
@@ -0,0 +1,32 @@
+/*
+Ini adalah Account.java yang merupakan sub class dari Mahasiswa.java.
+Lihat induk classnya (Mahasiswa.java)    : 
+Lihat pemanggilannya (aksesAccount.java) : 
+@author Summon Agus (L200130113)
+@blog bloggersmart.net
+*/
+ 
+public class Account extends Mahasiswa {
+	protected String username;
+	protected int password;
+	protected String deegree;
+	
+	public Account (String firstName, String lastName, String username, int password) {
+		super(firstName, lastName);
+		this.username = username;
+		this.password = password;
+	}
+	public Account (String firstName, String lastName, String deegree) {
+		super(firstName, lastName);
+		this.deegree = deegree;
+	}
+	public void infoAccount() {
+		System.out.println("Username : "+username);
+		System.out.println("Password : "+password);
+		super.getFullName();
+	}
+	public void getNameWithDegree() {
+		super.getFullName();
+		System.out.print(deegree);
+	}
+}
