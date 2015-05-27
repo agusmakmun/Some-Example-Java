c75d25cb3655a7365048101cc31bec17c2d0d772
agus makmun <summon.agus@gmail.com>
2015-05-28 01:28:43 +0700

    Create Mahasiswa.java

---
+++ Account-Info/Mahasiswa.java
@@ -0,0 +1,35 @@
+/*
+Ini adalah class Mahasiswa sebagai induk utamanya.
+Lihat sub classnya (Account.java)        : 
+Lihat pemanggilannya (aksesAccount.java) : 
+@author Summon Agus (L200130113)
+@blog bloggersmart.net
+*/
+ 
+public class Mahasiswa {
+	private String firstName, lastName, NIM, HBD, kelamin, alamat;
+	
+	public Mahasiswa(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	public Mahasiswa(String firstName, String lastName, String NIM, 
+					 String HBD, String kelamin, String alamat) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.NIM = NIM;
+		this.HBD = HBD;
+		this.kelamin = kelamin;
+		this.alamat = alamat;
+	}
+	protected void getFullName() {
+		System.out.print("Full Name: "+ firstName+" "+lastName); //tidak menggunakan println
+	}
+	public void infoMahasiswa() {
+		System.out.println("Nama    : "+firstName+" "+lastName);
+		System.out.println("NIM     : "+NIM);
+		System.out.println("HBD     : "+HBD);
+		System.out.println("Kelamin : "+kelamin);
+		System.out.println("Alamat  : "+alamat);
+	}
+}
