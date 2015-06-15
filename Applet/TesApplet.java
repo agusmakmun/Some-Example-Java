ed5537a0b15defe2ca9cb866080c87e15474f94e
agus makmun <summon.agus@gmail.com>
2015-06-15 08:31:48 +0700

    Create TesApplet.java

---
+++ Applet/TesApplet.java
@@ -0,0 +1,30 @@
+
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+
+/**
+ * @author agaust
+ * @blog bloggersmart.net
+ */
+public class TesApplet extends Applet {
+    private Object fontMetrics;
+    @SuppressWarnings("empty-statment")
+    @Override
+    public void paint(Graphics grafik) {
+        Font font = new Font("sanserif", Font.BOLD, 20);
+        grafik.setFont(font);
+        grafik.setColor(Color.blue);
+        
+        int xPusat = this.getSize().width/4;
+        int yPusat = this.getSize().height/2;
+        
+        String ucapan = "Selamat Belajar JAVA Applet";
+        FontMetrics fontMetrics = this.getFontMetrics(font);
+        int posisiX = xPusat-(fontMetrics.stringWidth(ucapan)/2);
+        
+        grafik.drawString(ucapan, xPusat, yPusat);
+    }
+}
